58fd7990ef20316c9af3a6104b4f09051f7d77df
briodan <41130293+briodan@users.noreply.github.com>
2025-05-15 16:20:37 +0000

    typo fix

--- custom_components/bambu_lab_p1_spaghetti_detection/const.py
+++ custom_components/bambu_lab_p1_spaghetti_detection/const.py
@@ -1,5 +1,5 @@
 #DOMAIN
-DOMAIN = "ha_bambu_lab_p1_spaghetti_detection"
+DOMAIN = "bambu_lab_p1_spaghetti_detection"
 # Device
 BRAND = "Bambu Lab P1 - Spaghetti Detection"
 DEVICE_NAME = "HA Spaghetti Detection"
